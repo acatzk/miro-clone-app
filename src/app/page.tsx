bf021cb9c2f8951206c9c959cda909e46c7c442b
acatzk <joshuaimalay@gmail.com>
2024-02-10 14:34:36 +0800

    feat: setup shadcn-ui re-usable componennts

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "~/components/ui/button";
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <p>Mero Page</p>
+      <Button variant="destructive">Button</Button>
+    </div>
+  );
+}
