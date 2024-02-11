baaf50314e919368c10978d20e1ad19757dec70f
Joshua Galit <joshuaimalay@gmail.com>
2024-02-11 13:15:43 +0800

    feat: setup dashboard layout (#4)
    * feat: setup dashboard layout
    
    * chore: remove build actions due to runtine edge

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+export const OrgSidebar = (): JSX.Element => {
+  return (
+    <div className="hidden w-[206px] flex-col space-y-6 bg-red-500 pl-5 pt-5 lg:flex">
+      Org Sidebar
+    </div>
+  )
+}
