3683c901fa86a59fbaf4789dc87f9daed37c0e53
acatzk <joshuaimalay@gmail.com>
2024-02-12 13:22:47 +0800

    feat: implement create board functionality

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,8 +1,34 @@
-import Image from 'next/image'
+'use client'
 
+import Image from 'next/image'
+import { toast } from 'sonner'
+import { useOrganization } from '@clerk/nextjs'
+
+import { api } from '~/convex/_generated/api'
 import { Button } from '~/components/ui/button'
+import { useApiMutation } from '~/hooks/use-api-mutation'
 
 export const EmptyBoards = (): JSX.Element => {
+  const { organization } = useOrganization()
+  const { mutate, pending } = useApiMutation(api.board.create)
+
+  const onClick = (): void => {
+    if (!organization) return
+
+    const promise = mutate({
+      orgId: organization?.id,
+      title: 'Untitled'
+    }).then((id) => {
+      // TODO: Redirect to board/{id}
+    })
+
+    toast.promise(promise, {
+      loading: 'Creating a new board...',
+      success: 'New board created!',
+      error: 'Failed to create a board.'
+    })
+  }
+
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <Image src="/note.svg" height={110} width={110} alt="Empty" />
@@ -11,7 +37,9 @@ export const EmptyBoards = (): JSX.Element => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button size="lg">Create board</Button>
+        <Button disabled={pending} size="lg" onClick={onClick}>
+          Create board
+        </Button>
       </div>
     </div>
   )
