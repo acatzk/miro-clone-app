f18f41b6c6284cd5d1bcfc3237a02dd4dad21b2d
acatzk <joshuaimalay@gmail.com>
2024-02-13 14:13:06 +0800

    feat: display board list data

---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { useAuth } from '@clerk/nextjs'
+import { formatDistanceToNow } from 'date-fns'
+
+import { Skeleton } from '~/components/ui/skeleton'
+
+import { Footer } from './footer'
+import { Overlay } from './overlay'
+
+type BoardCardProps = {
+  id: string
+  title: string
+  imageUrl: string
+  authorId: string
+  authorName: string
+  createdAt: number
+  orgId: string
+  isFavorite: boolean
+}
+
+export const BoardCard = (props: BoardCardProps): JSX.Element => {
+  const { id, title, imageUrl, authorId, authorName, createdAt, orgId, isFavorite } = props
+
+  const { userId } = useAuth()
+
+  const authorLabel = userId === authorId ? 'You' : authorName
+  const createdAtLabel = formatDistanceToNow(createdAt, {
+    addSuffix: true
+  })
+
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group flex aspect-[100/127] flex-col justify-between overflow-hidden rounded-lg border">
+        <div className="relative flex-1 bg-amber-50">
+          <Image src={imageUrl} alt={title} fill className="object-fit" />
+          <Overlay />
+        </div>
+        <Footer
+          isFavorite={isFavorite}
+          title={title}
+          authorLabel={authorLabel}
+          createdAtLabel={createdAtLabel}
+          onClick={() => {}}
+          disabled={false}
+        />
+      </div>
+    </Link>
+  )
+}
+
+BoardCard.Skeleton = function BoardCardSkeleton() {
+  return (
+    <div className="group aspect-[100/127] justify-between overflow-hidden rounded-lg">
+      <Skeleton className="h-full w-full" />
+    </div>
+  )
+}
