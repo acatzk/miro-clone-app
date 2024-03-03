89a9549da22b445f6240c0a5de74992a65fb0233
acatzk <joshuaimalay@gmail.com>
2024-03-03 12:54:36 +0800

    feat: create canvas layout

---
+++ app/board/[boardId]/_components/participants.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Participants = (): JSX.Element => {
+  return (
+    <div className="absolute right-2 top-2 flex h-12 items-center rounded-md bg-white p-3 shadow-md">
+      List of Users
+    </div>
+  )
+}
---
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Toolbar = (): JSX.Element => {
+  return (
+    <div className="absolute left-2 top-[50%] flex -translate-y-[50%] flex-col gap-y-4">
+      <div className="5 flex flex-col items-center gap-y-1 rounded-md bg-white p-1 shadow-md">
+        <div>Pencil</div>
+        <div>Square</div>
+        <div>Circle</div>
+        <div>Ellipsis</div>
+      </div>
+      <div className="flex flex-col items-center rounded-md bg-white p-1.5 shadow-md">
+        <div>Undo</div>
+        <div>Redo</div>
+      </div>
+    </div>
+  )
+}
